d1ea78f071c75b96a90cc6a368da6a072fd9178b
pauliyobo <pauliyobo@gmail.com>
2021-09-28 23:20:26 +0200

    updated examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,10 @@
-use cmd_rs::command::Command;
+use cmd_rs::command::Cmd;
 use cmd_rs::prompt::CommandLoop;
 
 fn main() {
     let cmd = CommandLoop::new()
         .with_intro("test intro")
         .with_prompt(">")
-        .add_command(Command::new("test", "test command"))
+        .add_command(Cmd::new("test", "test command"))
         .run();
 }
