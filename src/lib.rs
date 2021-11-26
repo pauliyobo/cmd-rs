c0a2fd0a6a2c371c8c1d2f27c1f9a1278da46129
pauliyobo <pauliyobo@gmail.com>
2021-11-26 20:39:16 +0100

    no longer need to install inventory in crates that use cmd-rs as a dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+pub extern crate inventory;
+
 pub mod command;
 mod command_processor;
 
 pub use self::command::RegisteredCommand;
 pub use self::command_processor::CommandProcessor;
-pub use inventory;
+pub use inventory as _inventory;
 pub use anyhow::Result;
 pub use cmd_rs_macro::make_command;
